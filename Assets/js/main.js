7edbdd9e8def42b64139255a78ec8be906ff723b
oc-garcia <octaviogarcia1988@gmail.com>
2023-01-15 10:02:44 -0300

    FInished Style HTML and API

---
+++ Assets/js/main.js
@@ -0,0 +1,26 @@
+let adviceNumber = document.getElementById("adviceNumber");
+let adviceField = document.getElementById("adviceField");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", advice);
+
+function advice() {
+  const url = "https://api.adviceslip.com/advice";
+
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Request failed with status code: " + response.status);
+      }
+    })
+    .then((data) => {
+      adviceNumber.innerHTML = `ADVICE # ${data.slip.id}`;
+      adviceField.innerHTML = `"${data.slip.advice}"`;
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
